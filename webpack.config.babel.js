b446a9b2d2092cfe5f60892f823bcf69cb74b6b7
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-10-17 21:35:24 +0900

    Create compile sass env

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -15,6 +15,10 @@ module.exports = [
           exclude: /node_modules/,
           loader: 'babel-loader',
         },
+        {
+          test: /\.sass?$/,
+          loaders: ['style', 'css', 'sass'],
+        },
       ],
     },
     resolve: {
